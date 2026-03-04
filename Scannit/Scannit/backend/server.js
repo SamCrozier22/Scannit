76138d52ecb3ffc2458be6e39b8a5672ccc9098d
450186 <146966554+450186@users.noreply.github.com>
2026-03-04 10:46:14 +0000

    setting up to host on render

--- Scannit/Scannit/backend/server.js
+++ Scannit/Scannit/backend/server.js
@@ -12,6 +12,10 @@ app.use((req, _res, next) => {
 
 const PORT = process.env.PORT || 5050;
 
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+})
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get('/health', (req, res) => {
