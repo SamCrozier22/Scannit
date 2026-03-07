a601e9950b3968c803a9bb912eb06c21bb267e42
450186 <146966554+450186@users.noreply.github.com>
2026-03-07 17:15:37 +0000

    Added User database logic

---
+++ Scannit/Scannit/backend/Models/Users.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const { Schema, model } = mongoose;
+
+const UserSchema = new Schema(
+    {
+        username: {type: String, required: true, unique: true, trim: true},
+        password: {type: String, required: true, trim: true},
+        firstName: {type: String, required: true, trim: true},
+        lastName: {type: String, required: true, trim: true},
+        email: {type: String, required: true, unique: true, trim: true},
+    },
+    {timestamps: true}
+);
+const userData = model("User", UserSchema);
+
+async function createUser(username, password, firstName, lastName, email) {
+    if(!username || !password || !firstName || !lastName || !email) {
+        throw new Error("Missing required fields");
+    }
+
+    try {
+        const userExists = await userData.findOne({username: username});
+        if(userExists) {
+            throw new Error("User already exists");
+        }
+        const newUser = await userData.create(username, password, firstName, lastName, email);
+        return newUser;
+    } catch (e) {
+        throw e
+    }
+}
+
+module.exports = {
+    userData,
+    createUser,
+}
